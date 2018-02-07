e1ea4704e326e372f383ec79e7baa2811115553d
Tim <NBK_TIM@163.com>
2018-02-07 11:27:27 +0800

    rename file name to if_statement.py

---
+++ template01/if_statement.py
@@ -0,0 +1,19 @@
+#encoding: utf-8
+
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/<is_login>')
+def index(is_login):
+    if is_login == '1':
+        user = {
+            'username':"Tim",
+            'age':18
+        }
+        return render_template('if_statement.html',user=user)
+    else:
+        return render_template('if_statement.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
